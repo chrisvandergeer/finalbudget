516f3c1e03e87a3acb87d7fc35da11d41d316149
Chris van der Geer <chris@MacBook-Air-van-Chris.local>
2018-11-25 00:00:52 +0100

    help optie aangevuld

--- src/main/java/nl/cge/budget/cmd/HelpCommand.java
+++ src/main/java/nl/cge/budget/cmd/HelpCommand.java
@@ -11,5 +11,15 @@ public class HelpCommand implements Command {
     public void execute(String command) {
         System.out.println("help\ttoont deze help");
         System.out.println("exit\tsluit dhe applicatie");
+        System.out.println("import\timporteert transakties");
+        System.out.println("find\tzoeken naar transakties, opties:");
+        System.out.println("\t\t -b zoeken op tegenrekeningnummer / naam");
+        System.out.println("\t\t -o zoeken op omschrijving");
+        System.out.println("\t\t -j zoeken op jaar");
+        System.out.println("\t\t -m zoeken op maand");
+        System.out.println("\t\t -d zoeken op dag");
+        System.out.println("\t\t -t zoeken op tag");
+        System.out.println("\t\t -T toevoegen tag");
+        System.out.println("\t\t -D verwijderen tag");
     }
 }
